97ab3574f6320dff54329d32a1e4fb528cade924
Tobias N <tobiasn.@Tobiass-MacBook-Pro.local>
2025-04-14 19:02:46 -0700

    .

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # app.py
 import streamlit as st
+st.set_page_config(page_title="Anchor Journal", layout="centered")
 from pages.auth_ui import login_signup_flow
 from pages.journal import render_journal_entry_form
 from pages.chat import render_chat_interface
 from utils.session_manager import init_session_state, logout_handler
 
-st.set_page_config(page_title="Anchor Journal", layout="centered")
+#st.set_page_config(page_title="Anchor Journal", layout="centered")
 st.title("🧠 Anchor Journal Portal")
 
 init_session_state()
