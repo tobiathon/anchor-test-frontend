12383d602dba6c5e8bc5f9f5a1e3863a0822ac21
Tobias N <tobiasn.@Tobiass-MacBook-Pro.local>
2025-05-05 21:05:53 -0700

    “.”

--- utils/session_manager.py
+++ utils/session_manager.py
@@ -1,14 +1,13 @@
-# File: frontend/utils/session_manager.py
+# utils/session_manager.py
 
 import time
 import streamlit as st
 
-
-def init_session_state(cookies):
+def init_session_state():
     if "token" not in st.session_state:
-        st.session_state["token"] = cookies.get("token") if cookies.get("token") else None
+        st.session_state["token"] = None
     if "username" not in st.session_state:
-        st.session_state["username"] = cookies.get("username") if cookies.get("username") else None
+        st.session_state["username"] = None
     if "chat_history" not in st.session_state:
         st.session_state["chat_history"] = []
     if "rerun_triggered" not in st.session_state:
